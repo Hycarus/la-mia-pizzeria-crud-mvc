cb3f22799c597422da3954f5e1376e6ec1b0c0fd
Hycarus <mirkomarasco08@gmail.com>
2024-05-20 11:36:56 +0200

    fixed modal

--- la-mia-pizzeria-razor-layout/wwwroot/js/site.js
+++ la-mia-pizzeria-razor-layout/wwwroot/js/site.js
@@ -1,32 +1,19 @@
-﻿//document.addEventListener("DOMContentLoaded", function () {
-//    const buttons = document.querySelectorAll(".cancel-button");
-//    buttons.forEach((button) => {
-//        button.addEventListener("click", (event) => {
-//            const dataId = button.getAttribute("data-id");
-//            const dataName = button.getAttribute("data-name");
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var itemModal = document.getElementById('deleteModal');
+    var itemForm = document.getElementById('itemForm');
+    var modalTitle = itemModal.querySelector('.modal-title');
+    var itemIdInput = document.getElementById('itemId');
+    var itemNameInput = document.getElementById('itemName');
 
-//            const modal = document.getElementById("deleteModal");
-//            const bootstrapModal = new bootstrap.Modal(modal);
-//            bootstrapModal.show();
+    itemModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget; 
+        var id = button.getAttribute('data-id'); 
+        var name = button.getAttribute('data-name');
+        var actionUrl = button.getAttribute('data-action-url');
 
-//            const title = document.getElementById("modal-item-title");
-//            title.textContent = dataName;
-
-//            const form = document.getElementById("deleteForm");
-//            form.setAttribute("action", `/Pizza/Delete/${dataId}`);
-//        });
-//    });
-//});
-
-//$.ajax({
-//    url: '@Url.Action("Delete", "Pizza")', // Assicurati che l'azione e il controller siano corretti
-//    type: 'POST',
-//    data: { id: pizzaId },
-//    success: function () {
-//        $('#deletePizzaModal').modal('hide');
-//        location.reload();
-//    },
-//    error: function () {
-//        alert('Errore nella cancellazione.');
-//    }
-
+        modalTitle.textContent = 'Sei sicuro di voler cancellare ' + name + '?';
+        itemIdInput.value = id;
+        itemNameInput.value = name;
+        itemForm.action = actionUrl;
+    });
+});
